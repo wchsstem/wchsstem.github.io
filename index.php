8ffef114b766d0a4e481843aa32e62127d01cd2a
nvarner <nathanmvarner@protonmail.com>
2018-09-08 13:07:34 -0400

    Added first sponsor

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $path_to_root = '';
-$extras = '<link rel="stylesheet" href="main.css"/>';
+$extras = '<link rel="stylesheet" href="main.css"/><link rel="stylesheet" href="sponsors/sponsor.css"/>';
 $description = 'Home page of the Winston Churchill High School STEM Club. Learn about the club, its members, and FTC.';
 require('templates/head.php');
 require('main.html');
